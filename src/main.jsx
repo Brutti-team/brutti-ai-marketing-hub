c8f5a773fad5bd29d933f08f48be323dc897fe27
michelleclairea22-ai <michelle.clairea22@gmail.com>
2026-08-21 16:41:18 +0800

    Mount Content Studio visual context guard

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import DailyContentRecommendationEnhancer from './DailyContentRecommendationEnha
 import HistoricalPostingTimeEnhancer from './HistoricalPostingTimeEnhancer.jsx'
 import BruttiSoulSystemEnhancer from './BruttiSoulSystemEnhancer.jsx'
 import ContentStudioController from './ContentStudioController.jsx'
+import ContentStudioAssetContextGuard from './ContentStudioAssetContextGuard.jsx'
 import './styles.css'
 import './core7-marketing-tools.css'
 import './box-polish.css'
@@ -122,6 +123,7 @@ createRoot(document.getElementById('root')).render(
     <DailyContentRecommendationEnhancer />
     <HistoricalPostingTimeEnhancer />
     <ContentStudioController />
+    <ContentStudioAssetContextGuard />
     <BruttiSoulSystemEnhancer />
     <DeferredEnhancers />
   </StrictMode>,
